b6a0571003614a673e85b8cf122b4e9d8c9d63db
sharib54009 <mohammedsharib0704@gmail.com>
2026-06-11 16:54:25 +0530

    Fix CORS and credentials

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)cd
+CORS(app, supports_credentials=True)
 
 app.config['SECRET_KEY'] = 'viceversa-secret'
 
